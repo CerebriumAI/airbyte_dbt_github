f3de72daec03d0b40f2a65c1a2007226c388f0b8
Elijah Roussos <elijah.rou@gmail.com>
2022-04-28 14:47:33 -0700

    deserialise json fields

--- models/staging/github_commits.sql
+++ models/staging/github_commits.sql
@@ -14,21 +14,50 @@ pr AS (
 	FROM
 		{{ var('pull_requests') }}
 	LEFT JOIN pr_commits USING(number)
+),
+
+commits_authors AS (
+	SELECT
+		_airbyte_commits_hashid,
+		id as author_id,
+		type as author_type,
+		login as author_username
+	FROM {{ var('commits_author') }}
+),
+
+
+commits_committers AS (
+	SELECT
+		_airbyte_commits_hashid,
+		type as committer_type,
+		login as committer_username
+	FROM {{ var('commits_committer') }}
+),
+
+commits_commit AS (
+	SELECT
+		_airbyte_commits_hashid,
+		comment_count,
+		message
+	FROM {{ var('commits_commit') }}
 )
 
 SELECT
-	sha as commit_hash,
+    sha,
 	created_at,
 	repository,
 	pr_id,
 	pr_number,
-	author->>'id' as author_id,
-	author->>'type' as author_type,
-	author->>'login' as author_username,
-	committer->>'type' as committer_type,
-	committer->>'login' as comitter_username,
-	commit->>'comment_count' as comment_count,
-	commit->>'message' as message
+	author_id,
+	author_type,
+	author_username,
+	committer_type,
+	committer_username,
+	comment_count,
+	message
 FROM
-	{{ var('commits') }}.commits
+	{{ var('commits') }}
 LEFT JOIN pr USING(sha)
+LEFT JOIN commits_authors USING(_airbyte_commits_hashid)
+LEFT JOIN commits_committers USING(_airbyte_commits_hashid)
+LEFT JOIN commits_commit USING(_airbyte_commits_hashid)
