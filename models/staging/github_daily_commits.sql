818c28fd39ad5e7534ea4c153ee6fb294feb5c5e
Elijah Roussos <elijah.rou@gmail.com>
2022-04-28 14:54:31 -0700

    deserialise daily commits

--- models/staging/github_daily_commits.sql
+++ models/staging/github_daily_commits.sql
@@ -1,13 +1,47 @@
+WITH commits_authors AS (
+	SELECT
+		_airbyte_commits_hashid,
+		id as author_id,
+		type as author_type,
+		login as author_username
+	FROM {{ var('commits_author') }}
+),
+
+
+commits_committers AS (
+	SELECT
+		_airbyte_commits_hashid,
+		type as committer_type,
+		login as committer_username
+	FROM {{ var('commits_committer') }}
+),
+
+commits AS (
+    SELECT
+        sha,
+        created_at,
+        repository,
+        author_id,
+        author_type,
+        author_username,
+        committer_type,
+        committer_username
+    FROM {{ var('commits_commit') }}
+    LEFT JOIN commits_authors USING(_airbyte_commits_hashid)
+    LEFT JOIN commits_committers USING(_airbyte_commits_hashid)
+)
+
 SELECT
 	date_trunc('day', created_at) AS date,
 	repository,
-	author->>'id' as author_id,
-	author->>'type' as author_type,
-	author->>'login' as author_username,
-	committer->>'type' as committer_type,
-	committer->>'login' as comitter_username,
+	author_id,
+	author_type,
+	author_username,
+	committer_type,
+	comitter_username,
 	count(*) as commits
 FROM
-	{{ var('commits') }}
+	commits
+LEFT JON
 GROUP BY
 	1,2,3,4,5,6
