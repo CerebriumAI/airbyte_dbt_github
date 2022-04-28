3e5da953dc0756300a78ec890230801f647e39cd
Elijah Roussos <elijah.rou@gmail.com>
2022-04-28 14:07:00 -0700

    commits table

---
+++ models/staging/github_commits.sql
@@ -0,0 +1,34 @@
+WITH pr_commits AS (
+	SELECT
+		sha,
+		pull_number as number
+	FROM
+		{{ var('pull_request_cmmits') }}.pull_request_commits
+),
+
+pr AS (
+	SELECT
+		id AS pr_id,
+		number as pr_number,
+		sha
+	FROM
+		{{ var('pull_requests') }}.pull_requests
+	LEFT JOIN pr_commits USING(number)
+)
+
+SELECT
+	sha as commit_hash,
+	created_at,
+	repository,
+	pr_id,
+	pr_number,
+	author->>'id' as author_id,
+	author->>'type' as author_type,
+	author->>'login' as author_username,
+	committer->>'type' as committer_type,
+	committer->>'login' as comitter_username,
+	commit->>'comment_count' as comment_count,
+	commit->>'message' as message
+FROM
+	{{ var('commits') }}.commits
+LEFT JOIN pr USING(sha)
